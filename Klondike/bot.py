fb480620d030007c165357144c8bddd584c445fb
Liza <126976897+Lizochka2005@users.noreply.github.com>
2024-10-04 17:25:30 +0300

    Add files via upload
    Всё кроме напоминалки, закомменчены по датам ограничения

--- Klondike/bot.py
+++ Klondike/bot.py
@@ -6,14 +6,14 @@ from table import *
 from info_psrticipants import *
 
 
-main_keyboard = [['Мое расписание', 'Расписание других организаторов', 'Информация об участнике'],
-                 ['Подключить напоминания', 'Отключить напоминания']]
+main_keyboard = [['Мое расписание', 'Расписание других организаторов', 'Информация об участнике']]
+                 # ,['Подключить напоминания', 'Отключить напоминания']]
 my_schedule_keyboard = [['Текущая точка', 'Дальнейшее расписание', 'Все расписание'], ['Назад']]
 organizers_schedule_keyboard = [['Расписание всего отдела', 'Расписание конкретного организатора'], ['Назад']]
 organizers_info_keyboard = [['По фамилии', 'По нику в ТГ'], ['Назад']]
 participant_info_keyboard = [['По фамилии', 'По нику в ТГ', 'По номеру комнаты'], ['Назад']]
 
-reminder_users = set()
+reminder_users = []
 
 
 async def start(update: Update, context: CallbackContext) -> None:
@@ -51,14 +51,15 @@ async def message_handler(update: Update, context: CallbackContext) -> None:
             )
             context.user_data['state'] = None
 
-    elif text == 'Подключить напоминания':
-        reminder_users.add(username)
-        await update.message.reply_text(
-            "Бот будет напоминать за 10 минут до точки и в момент начала.")
-
-    elif text == 'Отключить напоминания':
-        reminder_users.discard(username)
-        await update.message.reply_text("Вы отключили напоминания.")
+    # elif text == 'Подключить напоминания':
+    #     reminder_users.append(username)
+    #     await update.message.reply_text(
+    #         "Бот будет напоминать за 10 минут, за 5 минут до точки и в момент начала.")
+    #     print(reminder_users)
+    #
+    # elif text == 'Отключить напоминания':
+    #     reminder_users.remove(username)
+    #     await update.message.reply_text("Вы отключили напоминания.")
 
     elif text == 'Расписание других организаторов':
         await update.message.reply_text(
@@ -145,6 +146,11 @@ async def message_handler(update: Update, context: CallbackContext) -> None:
         elif text == 'По номеру комнаты':
             await update.message.reply_text('Введите номер комнаты: ')
             context.user_data['state'] = 'room_num'
+        else:
+            await update.message.reply_text(
+                "Выберите действие:",
+                reply_markup=ReplyKeyboardMarkup(main_keyboard, one_time_keyboard=True, resize_keyboard=True)
+            )
 
     elif context.user_data.get('state') == 'participant_familia':
         familia = text
@@ -179,9 +185,10 @@ async def message_handler(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text("Команда не распознана.")
 
 
-async def reminder_job(context: CallbackContext) -> None:
-    current_time = datetime.now()
-    pass
+# async def reminder_job(update: Update, context: CallbackContext) -> None:
+#     username = update.message.from_user.username
+#     if username in reminder_users:
+#         await update.message.reply_text(remainder(username))
 
 
 if __name__ == '__main__':
@@ -190,7 +197,7 @@ if __name__ == '__main__':
     job_queue = JobQueue()
     job_queue.set_application(application)
 
-    job_queue.run_repeating(reminder_job, interval=60, first=10)
+    # job_queue.run_repeating(reminder_job, interval=60)
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
--- Klondike/table.py
+++ Klondike/table.py
@@ -1,76 +1,83 @@
 import pandas as pd
 import datetime
+import math
 
 dt = datetime.datetime.now()
 today = dt.date()
 time = dt.time()
 
 
-df = pd.read_excel(r'C:\Users\Лиза\Downloads\СЕТКА.xlsx', sheet_name = 'Лист1')
-n = 115 #количество значимых строк(кол-во оргов + первые 2)
+df = pd.read_excel(r'C:\Users\Лиза\Downloads\СЕТКА.xlsx', sheet_name = 'ЛИЧНЫЕ СЕТКИ')
+n = 97 #количество значимых строк(кол-во оргов + первые 2)
 
 arr = df.to_numpy()
-arr = arr[:n-1]
-name_columns = arr[0][9::]
+name_columns = arr[0][8::]
+arr = arr[1:n-1]
 
 def my_timetable_all(username):
     text = ''
     for el in arr:
-        if el[2][1::] == username:
+        if el[1][1::] == username:
             for i in range(len(name_columns)):
-                text += str(name_columns[i]) + ' ' + el[i+9] +'\n'
+                text += str(name_columns[i]) + ' ' + str(el[i+8]) +'\n'
             return text
 
 def my_timetable_continue(username):
     text = ''
-    if (today != '2024-10-09' and today != '2024-10-10') or (today == '2024-10-09' and time < datetime.time(9,30,0)):
-        text += 'Дальнейшее расписание: \n'
-        text += my_timetable_all(username)
-        return text
+    # if (today != '2024-10-09' and today != '2024-10-10') or (today == '2024-10-09' and time < datetime.time(9,30,0)):
+    #     text += 'Дальнейшее расписание: \n'
+    #     text += my_timetable_all(username)
+    #     return text
     for el in arr:
-        if el[2][1::] == username:
+        if el[1][1::] == username:
             text += 'Дальнейшее расписание: \n'
             for i in range(len(name_columns)):
-                if name_columns[i] >= time:
+                t = name_columns[i].split('-')[0]
+                t = datetime.datetime.strptime(t, '%H:%M').time()
+                if t >= time:
                     index = i
                     break
             for i in range(index, len(name_columns)):
-                text += str(name_columns[i]) + ' ' + el[i+9] +'\n'
+                text += str(name_columns[i]) + ' ' + str(el[i+8]) +'\n'
             return text
 
 def my_timetable_now(username):
     text = ''
-    if (today != '2024-10-09' and today != '2024-10-10') or (today == '2024-10-09' and time < datetime.time(9,30,0)):
-        text = 'Посвят ещё не начался!\n'
-        return text
+    # if (today != '2024-10-09' and today != '2024-10-10') or (today == '2024-10-09' and time < datetime.time(9,30,0)):
+    #     text = 'Посвят ещё не начался!\n'
+    #     return text
     for el in arr:
-        if el[2][1::] == username:
+        if el[1][1::] == username:
             for i in range(len(name_columns)-1):
-                if name_columns[i] <= time and name_columns[i+1] > time:
-                    text += str(name_columns[i]) + ' ' + el[i+9] +'\n'
+                start = name_columns[i].split('-')[0]
+                start = datetime.datetime.strptime(start, '%H:%M').time()
+                end = name_columns[i].split('-')[1]
+                end = datetime.datetime.strptime(end, '%H:%M').time()
+                if start <= time and end > time:
+                    text += str(name_columns[i]) + ' ' + str(el[i+8]) +'\n'
                     return text
 
 def timetable_by_familia(familia):
     text = ''
     for el in arr:
-        if el[0] == familia:
-            text += el[0] + ' ' + el[1] + '\n'
-            text += 'Отдел: ' + el[5] + '\n'
+        if familia in el[0].split():
+            text += el[0] + '\n'
+            text += 'Отдел: ' + str(el[3]) + '\n'
             text += 'Сейчас на точке: '
-            text += my_timetable_now(el[2][1::])
-            text += my_timetable_continue(el[2][1::])
+            text += my_timetable_now(el[1][1::])
+            text += my_timetable_continue(el[1][1::])
             return text
     return False
 
 def timetable_by_username(username):
     text = ''
     for el in arr:
-        if el[2][1::] == username:
-            text += el[0] + ' ' + el[1] + '\n'
-            text += 'Отдел: ' + el[5] + '\n'
+        if el[1][1::] == username:
+            text += el[0] + '\n'
+            text += 'Отдел: ' + str(el[3]) + '\n'
             text += 'Сейчас на точке: '
-            text += my_timetable_now(el[2][1::])
-            text += my_timetable_continue(el[2][1::])
+            text += my_timetable_now(el[1][1::])
+            text += my_timetable_continue(el[1][1::])
             return text
     return False
 
@@ -78,13 +85,31 @@ def timetable_department(name):
     text = str(name).upper() + '\n'
     flag = False
     for el in arr:
-        a = el[5].split(', ')
+        a = list(map(lambda x: x.lower(), el[3].split(',')))
         if str(name).lower() in a:
              flag = True
-             text += (el[0] + ' ' + el[1] + ': ').upper()
-             text += str(my_timetable_now(el[2][1::]))
+             text += (el[0] + ': ').upper()
+             text += str(my_timetable_now(el[1][1::]))
     if flag:
         return text
     return False
 
-
+# def remainder(username):
+#     text = ''
+#     for el in arr:
+#         if el[1][1::] == username:
+#             for i in range(len(name_columns)):
+#                 start = name_columns[i].split('-')[0]
+#                 start = datetime.datetime.strptime(start, '%H:%M').time()
+#                 difference = datetime.timedelta(start).total_seconds()
+#                 defference = math.floor(difference/60)
+#                 if defference == 10:
+#                     text += 'Через 10 минут у тебя запланированно: '
+#                     text += str(el[i])
+#                 if defference == 5:
+#                     text += 'Через 5 минут у тебя запланированно: '
+#                     text += str(el[i])
+#                 if defference == 0:
+#                     text += 'Сейчас у тебя запланированно: '
+#                     text += str(el[i])
+#     return text
